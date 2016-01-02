9b928ac819a6fbefde7af05feebc2f23ed41ccf9
Andreas <cormackandy@hotmail.com>
2016-01-02 16:58:43 +0000

    added 2 subfolders

---
+++ YourApp/app.py
@@ -0,0 +1,18 @@
+from flask import Flask,request,render_template,redirect
+
+
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def index():
+	message = "This is a flask bootstrap apache2 starter template"
+	return render_template("home.html",message=message)
+
+
+if __name__=="__main__":
+	app.run(debug=True,host='0.0.0.0',port=8000)
